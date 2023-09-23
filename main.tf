4b4eb6e1c31605d8e3755d2a6f657cf225d2e7c2
Karivemula Sanjeeva <78853337+sanjeeva6268@users.noreply.github.com>
2023-09-23 12:30:54 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,29 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+  backend "azurerm" {}
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "Fitnessgeek-rg"
+  location = var.location-rg
+  tags = {
+    "Application" = "DemoApp"
+  }
+}
+
+
